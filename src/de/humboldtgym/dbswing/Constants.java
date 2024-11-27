d914f66187f9c25dd467ba6c310959e9cc47db40
Filip Zieliński <philipp.zelinski@gmail.com>
2024-11-27 09:24:26 +0100

    feat(Constants): create constants

---
+++ src/de/humboldtgym/dbswing/Constants.java
@@ -0,0 +1,9 @@
+package de.humboldtgym.dbswing;
+
+import java.awt.Color;
+
+public final class Constants {
+	private Constants() { }
+	
+	public static final Color DB_BLUE_COLOR = new Color(10, 30, 110);
+}
