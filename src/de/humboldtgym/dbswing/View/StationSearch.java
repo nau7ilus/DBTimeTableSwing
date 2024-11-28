69b55594800a5132c2f09977eed6d21759e169af
Filip Zieliński <philipp.zelinski@gmail.com>
2024-11-28 11:34:01 +0100

    feat(StationSearch): use WordWrap and set font

--- src/de/humboldtgym/dbswing/View/StationSearch.java
+++ src/de/humboldtgym/dbswing/View/StationSearch.java
@@ -3,42 +3,48 @@ package de.humboldtgym.dbswing.View;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import java.awt.Color;
+import java.awt.Font;
+
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+
+import de.humboldtgym.dbswing.WordWrapJLabel;
+
 import static de.humboldtgym.dbswing.Constants.DB_BLUE_COLOR;
 
 public class StationSearch extends JPanel {
 
-	private static final long serialVersionUID = 1L;
-	private JTextField txtSucheNachEinem;
+	private JTextField txtSearch;
 	
 	public StationSearch() {
 		setBackground(DB_BLUE_COLOR);
 		setLayout(null);
 		
-		JLabel lblHeadline = new JLabel("Bahnhofstafel");
+		WordWrapJLabel lblHeadline = new WordWrapJLabel("Bahnhofstafel");
 		lblHeadline.setForeground(Color.WHITE);
-		lblHeadline.setBounds(38, 78, 81, 21);
+		lblHeadline.setBounds(41, 86, 115, 21);
+		lblHeadline.setFont(new Font("D-DIN", Font.BOLD, 17));
 		add(lblHeadline);
 		
-		JLabel lblActionDescription = new JLabel("Bahnhof auswählen und Informationen zu Mobilität, Ausstattung und Services enthalten");
+		WordWrapJLabel lblActionDescription = new WordWrapJLabel("Bahnhof auswählen und Informationen zu Mobilität, Ausstattung und Services enthalten");
+		lblActionDescription.setFont(new Font("D-DIN Bold", Font.PLAIN, 15));
 		lblActionDescription.setHorizontalAlignment(SwingConstants.LEFT);
 		lblActionDescription.setVerticalAlignment(SwingConstants.TOP);
 		lblActionDescription.setForeground(Color.WHITE);
-		lblActionDescription.setBounds(37, 110, 220, 36);
+		lblActionDescription.setBounds(41, 118, 326, 28);
 		add(lblActionDescription);
 		
 		txtSearch = new JTextField();
 		txtSearch.setToolTipText("Suche nach einem Bahnhof");
-		txtSearch.setBounds(38, 146, 198, 21);
+		txtSearch.setBounds(41, 157, 198, 21);
 		add(txtSearch);
 		txtSearch.setColumns(20);
 		
-		JLabel lblSearchHint = new JLabel("Wie zum Beispiel Berlin Hauptbahnhof, Düsseldorf Hbf oder Hamburg Hbf. Schleife ist auch ein schöner Bahnhof.");
+		WordWrapJLabel lblSearchHint = new WordWrapJLabel("Wie zum Beispiel Berlin Hauptbahnhof, Düsseldorf Hbf oder Hamburg Hbf. Schleife ist auch ein schöner Bahnhof.");
 		lblSearchHint.setHorizontalAlignment(SwingConstants.LEFT);
 		lblSearchHint.setVerticalAlignment(SwingConstants.TOP);
 		lblSearchHint.setForeground(Color.WHITE);
-		lblSearchHint.setBounds(38, 178, 272, 21);
+		lblSearchHint.setBounds(41, 189, 329, 28);
 		add(lblSearchHint);
 
 	}
