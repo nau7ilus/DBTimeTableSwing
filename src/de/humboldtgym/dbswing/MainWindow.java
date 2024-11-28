9ba3aba58da0733edeb321615bf24113af0e61c8
Filip Zieliński <philipp.zelinski@gmail.com>
2024-11-28 11:14:20 +0100

    feat(MainWindow): custom fonts loader

--- src/de/humboldtgym/dbswing/MainWindow.java
+++ src/de/humboldtgym/dbswing/MainWindow.java
@@ -1,8 +1,16 @@
 package de.humboldtgym.dbswing;
 
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.GraphicsEnvironment;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 import de.humboldtgym.dbswing.View.StationSearch;
 
@@ -28,6 +36,8 @@ public class MainWindow {
 	}
 
 	private void initialize() {
+		this.usingCustomFonts();
+		
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -36,6 +46,28 @@ public class MainWindow {
 		
 		frame.add(stationSearchPanel);
 		
+	}
+	
+	private void usingCustomFonts() {
+	    GraphicsEnvironment GE = GraphicsEnvironment.getLocalGraphicsEnvironment();
+	    List<String> AVAILABLE_FONT_FAMILY_NAMES = Arrays.asList(GE.getAvailableFontFamilyNames());
+	    try {
+	        List<File> LIST = Arrays.asList(
+	          new File("resources/fonts/D-DIN.otf"),
+	          new File("resources/fonts/D-DIN-Bold.otf"),
+	          new File("resources/fonts/D-DIN-Italic.otf")
+	        );
+	        for (File LIST_ITEM : LIST) {
+	            if (LIST_ITEM.exists()) {
+	                Font FONT = Font.createFont(Font.TRUETYPE_FONT, LIST_ITEM);
+	                if (!AVAILABLE_FONT_FAMILY_NAMES.contains(FONT.getFontName())) {
+	                   GE.registerFont(FONT);
+	                }
+	            }
+	        }
+	    } catch (FontFormatException | IOException exception) {
+	        JOptionPane.showMessageDialog(null, exception.getMessage());
+	    }
 	}
 
 }
