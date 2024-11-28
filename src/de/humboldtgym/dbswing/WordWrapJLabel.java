7715eeff163e85a8f81e1ba5fc20a884d7eafdab
Filip Zieliński <philipp.zelinski@gmail.com>
2024-11-28 11:14:31 +0100

    feat(JLabel): created super class for wrapped text

---
+++ src/de/humboldtgym/dbswing/WordWrapJLabel.java
@@ -0,0 +1,30 @@
+package de.humboldtgym.dbswing;
+
+import javax.swing.JLabel;
+
+public class WordWrapJLabel extends JLabel {
+	
+	public final static String HTML_WORD_WRAP = "<html><body style=\"text-align: justify;  text-justify: inter-word;\">%s</body></html>";
+	
+	private String notFormattedText = null;
+	
+	public WordWrapJLabel(String text) {
+		super(getHTMLString(text));
+		this.notFormattedText = text;
+	}
+	
+	@Override
+	public void setText(String text) {
+		this.notFormattedText = text;
+		super.setText(getHTMLString(text));
+	}
+	
+	public String getNotFormattedText() {
+		return this.notFormattedText;
+	}
+	
+	private static String getHTMLString(String text) {
+		return String.format(HTML_WORD_WRAP, text);
+	}
+
+}
