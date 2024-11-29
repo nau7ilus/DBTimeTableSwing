26c19220ab8a5a64a5c1b65b4cc9339f2e34aa70
nau7ilus <filip@zelinskyi.de>
2024-11-30 00:31:42 +0100

    chore(constants): added phrases for forms

--- src/de/humboldtgym/dbswing/Constants.java
+++ src/de/humboldtgym/dbswing/Constants.java
@@ -7,4 +7,11 @@ public final class Constants {
 
 	public static final Color DB_BLUE_PRIMARY_COLOR = new Color(25, 33, 118);
 	public static final Color DB_BLUE_600_COLOR = new Color(12, 57, 146);
+
+	public static final String SEARCH_HEADLINE = "Bahnhofstafel";
+	public static final String SEARCH_ACTION_DESCRIPTION = "Bahnhof auswählen und Informationen zu Mobilität, Ausstattung und Services enthalten";
+	public static final String SEARCH_HINT = "Wie zum Beispiel <b><u>Berlin Hauptbahnhof</u></b>, <b><u>Düsseldorf Hbf</u></b> oder <b><u>Hamburg Hbf</u></b>. <b><u>Schleife</u></b> ist auch ein schöner Bahnhof.";
+	public static final String SEARCH_RESULTS = "Suchergebnisse:";
+	public static final String SEARCH_INPUT_PLACEHOLDER = "Suche nach einem Bahnhof";
+	public static final String OPEN_TABLE = "Öffnen";
 }
