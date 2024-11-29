9b1a6f60531219eb42fc3830daa83c8ad0efcb10
nau7ilus <filip@zelinskyi.de>
2024-11-30 00:33:44 +0100

    feat(SearchResultsPanel): panel with results

---
+++ src/de/humboldtgym/dbswing/View/SearchResultsPanel.java
@@ -0,0 +1,59 @@
+package de.humboldtgym.dbswing.View;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static de.humboldtgym.dbswing.Constants.OPEN_TABLE;
+import static de.humboldtgym.dbswing.Constants.SEARCH_RESULTS;
+
+public class SearchResultsPanel extends JPanel {
+    public String[] resultsData;
+
+    public SearchResultsPanel(String[] resultsData) {
+        this.resultsData = resultsData;
+
+        setOpaque(false);
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        addTitle();
+        addResultsList();
+        addSubmitButton();
+    }
+
+    private void addTitle() {
+        JLabel resultsLabel = new JLabel(SEARCH_RESULTS);
+        resultsLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        resultsLabel.setForeground(Color.WHITE);
+        resultsLabel.setFont(new Font("D-DIN", Font.ITALIC, 16));
+        add(resultsLabel);
+        addMarginBottom(5);
+    }
+
+    private void addResultsList() {
+        JList<String> resultsList = new JList<>(this.resultsData);
+        resultsList.setFont(new Font("DIN-D", Font.PLAIN, 20));
+        resultsList.setFixedCellWidth(getWidth());
+        resultsList.setLayoutOrientation(JList.VERTICAL);
+
+        JScrollPane resultsScrollPane = new JScrollPane(resultsList);
+        resultsScrollPane.setMaximumSize(new Dimension(400, 200));
+        resultsScrollPane.setPreferredSize(new Dimension(400, 150));
+        resultsScrollPane.setAlignmentX(Component.LEFT_ALIGNMENT);
+        add(resultsScrollPane);
+        addMarginBottom(15);
+    }
+
+    private void addSubmitButton() {
+        JButton openButton = new JButton(OPEN_TABLE);
+        openButton.setFont(new Font("DIN-D", Font.PLAIN, 20));
+        openButton.setAlignmentX(Component.LEFT_ALIGNMENT);
+        openButton.setMinimumSize(new Dimension(400, 40));
+        add(openButton);
+        add(Box.createVerticalGlue());
+    }
+
+    private void addMarginBottom(int height) {
+        add(Box.createVerticalStrut(height));
+    }
+
+}
