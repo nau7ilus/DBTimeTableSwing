6b660f87db3bd6abdbb8c2f5c9b4617dbc740543
nau7ilus <filip@zelinskyi.de>
2024-11-30 00:31:28 +0100

    chore(constants): change blue shade color

--- src/de/humboldtgym/dbswing/Constants.java
+++ src/de/humboldtgym/dbswing/Constants.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 
 public final class Constants {
 	private Constants() { }
-	
-	public static final Color DB_BLUE_COLOR = new Color(10, 30, 110);
+
+	public static final Color DB_BLUE_PRIMARY_COLOR = new Color(25, 33, 118);
+	public static final Color DB_BLUE_600_COLOR = new Color(12, 57, 146);
 }
