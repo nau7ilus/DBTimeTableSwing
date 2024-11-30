fb17d89866f2fc51cf7f26d9030c77c5e3a06bb8
nau7ilus <filip@zelinskyi.de>
2024-11-30 02:11:01 +0100

    chore(Constants): added api endpoint to retrieve locations

--- src/de/humboldtgym/dbswing/Constants.java
+++ src/de/humboldtgym/dbswing/Constants.java
@@ -1,12 +1,14 @@
 package de.humboldtgym.dbswing;
 
-import java.awt.Color;
+import java.awt.*;
 
 public final class Constants {
 	private Constants() { }
 
 	public static final Color DB_BLUE_PRIMARY_COLOR = new Color(25, 33, 118);
 	public static final Color DB_BLUE_600_COLOR = new Color(12, 57, 146);
+
+	public static final String API_LOCATIONS = "https://v5.db.transport.rest/locations?&fuzzy=true&results=10&stops=true&addresses=false&poi=false&linesOfStops=false&language=de&query=";
 
 	public static final String SEARCH_HEADLINE = "Bahnhofstafel";
 	public static final String SEARCH_ACTION_DESCRIPTION = "Bahnhof auswählen und Informationen zu Mobilität, Ausstattung und Services enthalten";
