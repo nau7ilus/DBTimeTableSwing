317d29fc869186a9a6e0bca18b1cb70bc9a6687d
nau7ilus <filip@zelinskyi.de>
2024-11-30 02:11:43 +0100

    chore: lint

--- src/de/humboldtgym/dbswing/MainWindow.java
+++ src/de/humboldtgym/dbswing/MainWindow.java
@@ -1,27 +1,26 @@
 package de.humboldtgym.dbswing;
 
-import java.awt.EventQueue;
-import java.awt.Font;
-import java.awt.FontFormatException;
-import java.awt.GraphicsEnvironment;
+import de.humboldtgym.dbswing.Controller.SearchController;
+import de.humboldtgym.dbswing.Model.StationModel;
+import de.humboldtgym.dbswing.View.SearchView;
+
+import javax.swing.*;
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-
-import de.humboldtgym.dbswing.Controller.SearchController;
-import de.humboldtgym.dbswing.Model.StationModel;
-import de.humboldtgym.dbswing.View.SearchView;
-
 public class MainWindow {
 
-	private SearchView searchView;
+    private SearchView searchView;
 
-	public static void main(String[] args) {
-		EventQueue.invokeLater(() -> {
+    public MainWindow() {
+        initialize();
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
             try {
                 MainWindow window = new MainWindow();
                 window.searchView.setVisible(true);
@@ -29,40 +28,32 @@ public class MainWindow {
                 e.printStackTrace();
             }
         });
-	}
+    }
 
-	public MainWindow() {
-		initialize();
-	}
+    private void initialize() {
+        this.usingCustomFonts();
+        searchView = new SearchView();
+        StationModel stationModel = new StationModel();
+        new SearchController(stationModel, searchView);
 
-	private void initialize() {
-		this.usingCustomFonts();
-		searchView = new SearchView();
-		StationModel stationModel = new StationModel();
-		new SearchController(stationModel, searchView);
+    }
 
-	}
-	
-	private void usingCustomFonts() {
-	    GraphicsEnvironment GE = GraphicsEnvironment.getLocalGraphicsEnvironment();
-	    List<String> AVAILABLE_FONT_FAMILY_NAMES = Arrays.asList(GE.getAvailableFontFamilyNames());
-	    try {
-	        List<File> LIST = Arrays.asList(
-	          new File("resources/fonts/D-DIN.otf"),
-	          new File("resources/fonts/D-DIN-Bold.otf"),
-	          new File("resources/fonts/D-DIN-Italic.otf")
-	        );
-	        for (File LIST_ITEM : LIST) {
-	            if (LIST_ITEM.exists()) {
-	                Font FONT = Font.createFont(Font.TRUETYPE_FONT, LIST_ITEM);
-	                if (!AVAILABLE_FONT_FAMILY_NAMES.contains(FONT.getFontName())) {
-	                   GE.registerFont(FONT);
-	                }
-	            }
-	        }
-	    } catch (FontFormatException | IOException exception) {
-	        JOptionPane.showMessageDialog(null, exception.getMessage());
-	    }
-	}
+    private void usingCustomFonts() {
+        GraphicsEnvironment GE = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        List<String> AVAILABLE_FONT_FAMILY_NAMES = Arrays.asList(GE.getAvailableFontFamilyNames());
+        try {
+            List<File> LIST = Arrays.asList(new File("resources/fonts/D-DIN.otf"), new File("resources/fonts/D-DIN-Bold.otf"), new File("resources/fonts/D-DIN-Italic.otf"));
+            for (File LIST_ITEM : LIST) {
+                if (LIST_ITEM.exists()) {
+                    Font FONT = Font.createFont(Font.TRUETYPE_FONT, LIST_ITEM);
+                    if (!AVAILABLE_FONT_FAMILY_NAMES.contains(FONT.getFontName())) {
+                        GE.registerFont(FONT);
+                    }
+                }
+            }
+        } catch (FontFormatException | IOException exception) {
+            JOptionPane.showMessageDialog(null, exception.getMessage());
+        }
+    }
 
 }
