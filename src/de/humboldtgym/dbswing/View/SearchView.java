695268936fa68e1fcc68f22346b4c1dd071483cd
nau7ilus <filip@zelinskyi.de>
2024-11-30 01:32:23 +0100

    chore(SearchView): rename StationSearchFrame to SearchView

--- src/de/humboldtgym/dbswing/View/SearchView.java
+++ src/de/humboldtgym/dbswing/View/SearchView.java
@@ -1,12 +1,18 @@
 package de.humboldtgym.dbswing.View;
 
+import de.humboldtgym.dbswing.FancyTextField;
+import de.humboldtgym.dbswing.Model.Station;
+
 import javax.swing.*;
 import java.awt.*;
 
 import static de.humboldtgym.dbswing.Constants.DB_BLUE_PRIMARY_COLOR;
 
-public class StationSearchFrame extends JFrame {
-    public StationSearchFrame() {
+public class SearchView extends JFrame {
+    private final SearchPanel searchPanel = new SearchPanel();
+    private final SearchResultsPanel resultsPanel = new SearchResultsPanel();
+
+    public SearchView() {
         setBounds(0, 0, 900, 350);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Bahnhofstafel — Stationensuche");
@@ -17,17 +23,22 @@ public class StationSearchFrame extends JFrame {
         containerPanel.setBorder(BorderFactory.createEmptyBorder(40, 40, 20, 20));
         getContentPane().add(containerPanel);
 
-        SearchPanel searchPanel = new SearchPanel();
         searchPanel.setPreferredSize(new Dimension(400, 350));
         containerPanel.add(searchPanel);
 
         containerPanel.add(Box.createHorizontalStrut(20));
 
-        String[] data = {"Berlin Hauptbahnhof", "Hamburg Hbf", "Potsdam Hbf", "sdfsd", "dsfsfd", "dsfsdf", "sdfsdf"};
-        SearchResultsPanel resultsPanel = new SearchResultsPanel(data);
         resultsPanel.setPreferredSize(new Dimension(400, 350));
         containerPanel.add(resultsPanel);
 
         SwingUtilities.invokeLater(containerPanel::requestFocusInWindow);
+    }
+
+    public FancyTextField getSearchField() {
+        return searchPanel.getSearchInput();
+    }
+
+    public JList<Station> getResultsList() {
+        return resultsPanel.getResultsList();
     }
 }
