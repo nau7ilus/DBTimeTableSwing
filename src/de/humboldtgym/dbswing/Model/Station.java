d76a6ebe1d4e0ffa0d44647678da2cfbaa190714
nau7ilus <filip@zelinskyi.de>
2024-12-07 01:00:32 +0100

    feat(Station): add national and regional express

--- src/de/humboldtgym/dbswing/Model/Station.java
+++ src/de/humboldtgym/dbswing/Model/Station.java
@@ -2,9 +2,15 @@ package de.humboldtgym.dbswing.Model;
 
 import org.json.JSONObject;
 
+import java.time.Instant;
+import java.util.Date;
+
 public class Station {
     private final String id;
     private final String name;
+    private Date departure;
+    private boolean hasNationalExpress;
+    private boolean hasRegionalExpress;
 
     public Station(String id, String name) {
         this.id = id;
@@ -19,6 +25,22 @@ public class Station {
         return name;
     }
 
+    public Date getDeparture() {
+        return this.departure;
+    }
+
+    public boolean hasNationalExpress() {
+        return hasNationalExpress;
+    }
+
+    public boolean hasRegionalExpress() {
+        return hasRegionalExpress;
+    }
+
+    public void setDeparture(Date departure) {
+        this.departure = departure;
+    }
+
     @Override
     public String toString() {
         return this.name;
@@ -27,6 +49,13 @@ public class Station {
     public static Station parseJSONObject(JSONObject obj) {
         String id = obj.getString("id");
         String name = obj.getString("name");
-        return new Station(id, name);
+
+        Station station = new Station(id, name);
+
+        JSONObject products = obj.getJSONObject("products");
+        station.hasNationalExpress = products.getBoolean("nationalExpress");
+        station.hasRegionalExpress = products.getBoolean("regionalExp");
+
+        return station;
     }
 }
